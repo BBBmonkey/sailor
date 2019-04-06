d307a9ccf1621f7a4560a4aec0daa6e2855d5412
bbbmonkey <barak.baram@gmail.com>
2019-04-06 11:19:20 +0300

    implements filtering

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,57 +1,128 @@
 <template>
   <div>
     <el-autocomplete
-    v-model='state4'
-    :fetch-suggestions="querySearchAsync"
-    placeholder='Please input'
-    @select="handleSelect"
+      v-model="industry"
+      :fetch-suggestions="searchIndustriesAsync"
+      placeholder="Choose industry"
     ></el-autocomplete>
+    <el-autocomplete
+      v-model="solution"
+      :fetch-suggestions="searchSolutionsAsync"
+      placeholder="Choose solution"
+    ></el-autocomplete>
+
+    <div class="search-results">
+      <SalePrev v-for="prev in filteredPosts" :key="prev.key" :sale="prev"/>
+    </div>
   </div>
 </template>
 
 <script>
+import SalePrev from './salePrev';
+
 export default {
+  components: { SalePrev },
+
   data() {
     return {
-      links: [],
-      state4: '',
+      industries: [],
+      solutions: [],
+      industry: '',
+      solution: '',
       timeout: null,
+      salesPrevs: [
+        {
+          key: '1',
+          industry: 'Water',
+          solution: 'Breathing bags',
+          story: 'These breathing bags are great for using under water',
+        },
+        {
+          key: '2',
+          industry: 'Chemicals',
+          solution: 'Breathing bags',
+          story: 'These breathing bags are great for using with Chemicals',
+        },
+        {
+          key: '3',
+          industry: 'Water',
+          solution: 'Water heater',
+          story: 'This Water heater is great for using with water',
+        },
+      ],
     };
   },
+
+  computed: {
+    filteredPosts() {
+      const x = this.salesPrevs.filter(
+        prev =>
+          prev.industry.includes(this.industry) &&
+          prev.solution.includes(this.solution),
+      );
+      return x;
+    },
+  },
+
   methods: {
-    loadAll() {
+    loadIndustries() {
       return [
-        { value: 'vue', link: 'https://github.com/vuejs/vue' },
-        { value: 'element', link: 'https://github.com/ElemeFE/element' },
-        { value: 'cooking', link: 'https://github.com/ElemeFE/cooking' },
-        { value: 'mint-ui', link: 'https://github.com/ElemeFE/mint-ui' },
-        { value: 'vuex', link: 'https://github.com/vuejs/vuex' },
-        { value: 'vue-router', link: 'https://github.com/vuejs/vue-router' },
-        { value: 'babel', link: 'https://github.com/babel/babel' },
+        { value: 'aaa', link: 'https://github.com/vuejs/vue' },
+        { value: 'bbb', link: 'https://github.com/ElemeFE/element' },
+        { value: 'Water', link: 'https://github.com/babel/babel' },
+        { value: 'Chemicals', link: 'https://github.com/babel/babel' },
       ];
     },
 
-    querySearchAsync(queryString, cb) {
-      const links = this.links;
-      const results = queryString ? links.filter(this.createFilter(queryString)) : links;
+    loadSolutions() {
+      return [
+        { value: 'Breathing bags', link: 'https://github.com/vuejs/vue' },
+        { value: 'Water heater', link: 'https://github.com/ElemeFE/element' },
+        { value: 'Water', link: 'https://github.com/babel/babel' },
+        { value: 'Chemicals', link: 'https://github.com/babel/babel' },
+      ];
+    },
+
+    searchIndustriesAsync(queryString, cb) {
+      const links = this.industries;
+      const results = queryString
+        ? links.filter(this.createFilter(queryString))
+        : links;
 
       clearTimeout(this.timeout);
       this.timeout = setTimeout(() => {
         cb(results);
       }, 3000 * Math.random());
     },
-    createFilter(queryString) {
-      return (link) => {
-        return (link.value.toLowerCase().indexOf(queryString.toLowerCase()) === 0);
-      };
+
+    searchSolutionsAsync(queryString, cb) {
+      const links = this.solutions;
+      const results = queryString
+        ? links.filter(this.createFilter(queryString))
+        : links;
+
+      clearTimeout(this.timeout);
+      this.timeout = setTimeout(() => {
+        cb(results);
+      }, 3000 * Math.random());
     },
-    handleSelect(item) {
-      // eslint-disable-next-line
-      console.log(item);
+
+    createFilter(queryString) {
+      return link =>
+        link.value.toLowerCase().indexOf(queryString.toLowerCase()) === 0;
     },
   },
   mounted() {
-    this.links = this.loadAll();
+    this.industries = this.loadIndustries();
+    this.solutions = this.loadSolutions();
   },
 };
 </script>
+
+<style scoped>
+.search-results {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+}
+</style>
--- src/components/salePrev.vue
+++ src/components/salePrev.vue
@@ -1,14 +1,46 @@
-<template></template>
+<template>
+  <div class="sale-prev">
+    <header>
+      <div class="industry">{{ this.sale.industry }}</div>
+      <div class="solution">{{ this.sale.solution }}</div>
+    </header>
+    <div class="story">{{ this.sale.story }}</div>
+  </div>
+</template>
 
 <script>
 export default {
   data() {
     return {
-      quote: 'I want to be single'
-    }
-  }
-}
+      quote: 'I want cookies',
+    };
+  },
+
+  props: ['sale'],
+};
 </script>
 
-<style>
+<style scoped>
+.sale-prev {
+  display: flex;
+  flex-direction: column;
+  margin: 10px;
+  background-color: #fff;
+  box-shadow: 1px 2px #e4e4e4;
+  width: 200px;
+  height: 300px;
+  padding: 10px;
+}
+
+header {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+}
+
+.story {
+  text-overflow: ellipsis;
+  overflow: hidden;
+  white-space: nowrap;
+}
 </style>
